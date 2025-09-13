db8e5455d19221a059accaaa7c0f3ddac6dcb657
matteovalerio <matteovalerio2006@hotmail.it>
2025-09-13 17:04:15 +0200

    feat: add start example

---
+++ src/main/kotlin/start/Main.kt
@@ -0,0 +1,53 @@
+package main.kotlin.start
+
+import main.kotlin.end.Runner
+
+import java.io.InvalidClassException
+
+
+fun main() {
+//    val s = suite("Demo DSL Test") {
+//        test("Basic sum tests") {
+//            assert("1 + 1 should be 2") {
+//                1 + 1 shouldBe 2
+//            }
+//            assert("1 + 1 should be 3") {
+//                1 + 1 shouldBe 3
+//            }
+//        }
+//
+//        test("Nullability tests") {
+//            assert("null should be null") {
+//                val test: String? = null
+//                test.shouldBeNull()
+//            }
+//            assert("DevFest should not be null") {
+//                "DevFest".shouldNotBe(null)
+//            }
+//        }
+//
+//        test("Should contain") {
+//            assert("DevFest should contain est") {
+//                "DevFest" shouldContain "est"
+//            }
+//            assert("[1,2,3] should contain 3") {
+//                listOf(1,2,3) shouldContain 3
+//            }
+//        }
+//
+//        test("Exceptions ") {
+//            assert("listOf(1).[2] throw IndexOutOfBounds") {
+//                shouldThrow<IndexOutOfBoundsException> {
+//                    listOf(1)[2]
+//                }
+//            }
+//            assert("listOf(1).[2] throw InvalidClassException") {
+//                shouldThrow<InvalidClassException> {
+//                    listOf(1)[2]
+//                }
+//            }
+//        }
+//    }
+//
+//    Runner.run(s)
+}
---
+++ src/main/kotlin/start/builders/TestSuiteBuilder.kt
@@ -0,0 +1,4 @@
+package main.kotlin.start.builders
+
+class TestSuiteBuilder {
+}
